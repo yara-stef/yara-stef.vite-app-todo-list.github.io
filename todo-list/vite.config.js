cd6059f83d7e0ff3345aa8f423d084718d61e7ff
yara-stef <ymhladyak@gmail.com>
2023-02-27 09:23:57 +0000

    hmr

--- todo-list/vite.config.js
+++ todo-list/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+const config = () => ({
+  server: {
+    hmr: {
+      clientPort: ({}).HMR_HOST ? 443 : 5173,
+      host: ({}).HMR_HOST
+        ? new URL(({}).HMR_HOST).hostname
+        : "localhost",
+    },
+  },
+});
+
+export default config;
